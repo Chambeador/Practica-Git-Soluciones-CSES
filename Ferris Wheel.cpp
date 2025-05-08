7f5bd2d1b76103c9316925061af2edace83916fd
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2025-05-08 00:08:14 -0400

    feat(ferris-wheel): initialize variables and sort array

--- Ferris Wheel.cpp
+++ Ferris Wheel.cpp
@@ -4,8 +4,10 @@ using namespace std;
 void solve(){
      int n,k; cin>>n>>k;
      vector<int> arr(n);
-     
+     int ans = 0;
+     int l = 0, r = n-1;
+     sort(arr.begin(), arr.end());
 }
 signed main(){
-
+       solve();
 }
