1c1ac9ef0a84456dbfd5c3375911eb04bf5e5ca9
Adriana <soto7adriana@gmail.com>
2025-05-08 14:42:26 -0400

    feat(repetitions): implement algorithm to find longest repetition

--- repetitions.cpp
+++ repetitions.cpp
@@ -4,6 +4,17 @@ using namespace std;
 void solve() {
     string s;
     cin >> s;
+
+    int max_count = 1; 
+    int current = 1;
+    for (int i = 1; i < s.size(); ++i) {
+        if (s[i] == s[i-1]) {
+            current++;
+            max_count = max(max_count, current);
+        } else {
+            current = 1;
+        }
+    }
 }
 
 int main() {
