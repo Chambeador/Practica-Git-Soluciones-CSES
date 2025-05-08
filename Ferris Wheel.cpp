1ca918c28625b30fc1378637254d734515f03351
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2025-05-08 00:11:15 -0400

    feat(ferris-wheel): implement two-pointer logic

--- Ferris Wheel.cpp
+++ Ferris Wheel.cpp
@@ -7,6 +7,19 @@ void solve(){
      int ans = 0;
      int l = 0, r = n-1;
      sort(arr.begin(), arr.end());
+     while(l <= r){ 
+           if(l == r){
+               ans++; break;
+           }
+           if(arr[l]+arr[r] > k){
+               ans++;
+               r--;
+            }else{
+               ans++;
+               l++,r--;
+            }    
+     }
+     cout<<ans<<endl;
 }
 signed main(){
        solve();
