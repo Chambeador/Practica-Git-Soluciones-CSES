73a33e0d69e4876f2c84627478f245f35f1b6e40
Maite <acostaorellanad@gmail.com>
2025-05-08 12:09:31 -0400

    feat  (Increasin Array): add loop to count minimum moves

--- Increasing Array.cpp
+++ Increasing Array.cpp
@@ -2,9 +2,18 @@
 #define int long long
 using namespace std;
 void solve(){
+
     int n; cin>>n;
     vector<int> arr(n);
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
+
+    int c = 0;
+    for(int i = 1; i < n; i++){
+            if(arr[i] < arr[i-1]){
+                    c += arr[i-1]-arr[i];
+                    arr[i] = arr[i-1];
+                }
+        }
     
