bc82bd100906c490960131709eca0cbb6e71de60
Maite <acostaorellanad@gmail.com>
2025-05-08 11:57:12 -0400

    feat (Increasing Array): read input size and array

--- Increasing Array.cpp
+++ Increasing Array.cpp
@@ -1,3 +1,10 @@
 #include <bits/stdc++.h> 
 #define int long long
 using namespace std;
+void solve(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    
